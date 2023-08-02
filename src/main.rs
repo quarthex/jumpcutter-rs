02ddc2be05be53c51733962565c27c6f0109fa7a
Romain DÉOUX <romain.deoux@gmail.com>
2023-08-02 22:55:12 +0200

    bump dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,33 @@
-use clap::Arg;
+use clap::Parser;
 use log::{debug, error, info};
 use simple_logger::SimpleLogger;
 use std::fs::{canonicalize, File};
 use std::io::{BufRead, BufReader, Write};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::process::{exit, Command, Stdio};
+
+#[derive(Parser)]
+#[command(dont_collapse_args_in_usage = true)]
+struct Args {
+    #[arg(long)]
+    tempdir: Option<PathBuf>,
+    input_file: PathBuf,
+    output_file: PathBuf,
+}
 
 fn main() {
     SimpleLogger::new().init().unwrap_or_default();
-    let app = clap::Command::new("jumpcutter")
-        .dont_collapse_args_in_usage(true)
-        .arg(Arg::new("tempdir").long("--tempdir").takes_value(true))
-        .arg(Arg::new("input-file").required(true))
-        .arg(Arg::new("output-file").required(true));
-    let matches = app.get_matches();
-    let input_file = Path::new(matches.value_of_os("input-file").unwrap_or_default());
-    let output_file = Path::new(matches.value_of_os("output-file").unwrap_or_default());
+    let args = Args::parse();
 
-    if output_file.exists() {
-        error!("{:?} already exists", output_file);
+    if args.output_file.exists() {
+        error!("{:?} already exists", args.output_file);
         exit(1)
     }
 
     info!("Create temporary directory");
-    let tempdir = matches
-        .value_of_os("tempdir")
+    let tempdir = args
+        .tempdir
+        .as_ref()
         .map_or_else(tempfile::tempdir, tempfile::tempdir_in)
         .unwrap_or_else(|err| {
             error!("Failed to create a temporary directory");
@@ -39,10 +42,10 @@ fn main() {
         exit(err.raw_os_error().unwrap_or(1))
     });
     writeln!(&concat_script, "ffconcat version 1.0").expect("Failed to write");
-    let input_file = canonicalize(input_file).unwrap_or_else(|err| {
+    let input_file = canonicalize(&args.input_file).unwrap_or_else(|err| {
         error!(
             "Failed to get the canonical path of {:?}: {}",
-            input_file, err
+            args.input_file, err
         );
         exit(err.raw_os_error().unwrap_or(1))
     });
@@ -51,8 +54,8 @@ fn main() {
     let mut ffmpeg = Command::new("ffmpeg")
         .arg("-i")
         .arg(&input_file)
-        .args(&["-af", "silencedetect=n=0.03:d=0.1"])
-        .args(&["-f", "null"])
+        .args(["-af", "silencedetect=n=0.03:d=0.1"])
+        .args(["-f", "null"])
         .arg("-")
         .stderr(Stdio::piped())
         .spawn()
@@ -63,7 +66,7 @@ fn main() {
     if let Some(output) = ffmpeg.stderr.take() {
         let output = BufReader::new(output);
         let mut silence_end = 0.0;
-        for (uniq, line) in output.lines().filter_map(Result::ok).enumerate() {
+        for (uniq, line) in output.lines().map_while(Result::ok).enumerate() {
             eprintln!("{}", &line);
             if let Some(pos) = line.find("silence_start: ") {
                 if let Some(silence_start) = line[pos..].split_whitespace().nth(1) {
@@ -71,7 +74,7 @@ fn main() {
                         if (silence_start - silence_end).abs() > 0.01 {
                             debug!("keep {}-{}", silence_end, silence_start);
                             let mut piece = tempdir.path().to_owned();
-                            piece.push(format!("piece-{:08x}.mkv", uniq));
+                            piece.push(format!("piece-{uniq:08x}.mkv"));
                             writeln!(concat_script, "file {}", piece.to_string_lossy())
                                 .expect("Failed to write");
                             slice(silence_end, silence_start - silence_end, &input_file, piece);
@@ -91,7 +94,7 @@ fn main() {
     drop(concat_script); // Flush and close the script
 
     info!("Concatenate pieces");
-    concatenate(concat_script_path, output_file);
+    concatenate(concat_script_path, args.output_file);
 }
 
 fn slice<I, O>(timestamp: f32, duration: f32, input: I, output: O)
@@ -100,8 +103,8 @@ where
     O: AsRef<Path>,
 {
     let status = Command::new("ffmpeg")
-        .args(&["-ss", &timestamp.to_string()])
-        .args(&["-t", &duration.to_string()])
+        .args(["-ss", &timestamp.to_string()])
+        .args(["-t", &duration.to_string()])
         .arg("-i")
         .arg(input.as_ref())
         .arg(output.as_ref())
@@ -122,10 +125,10 @@ where
     O: AsRef<Path>,
 {
     let status = Command::new("ffmpeg")
-        .args(&["-f", "concat", "-safe", "0"])
+        .args(["-f", "concat", "-safe", "0"])
         .arg("-i")
         .arg(input.as_ref())
-        .args(&["-c", "copy"])
+        .args(["-c", "copy"])
         .arg(output.as_ref())
         .status()
         .unwrap_or_else(|err| {
